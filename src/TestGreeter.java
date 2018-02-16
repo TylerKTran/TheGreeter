c98410b7b722006b1aacbbe90fbdc569a6d2b924
Unknown <19trant@elmbrookstudents.org>
2018-02-16 12:02:20 -0600

    States in an Array

--- src/TestGreeter.java
+++ src/TestGreeter.java
@@ -2,8 +2,12 @@
 public class TestGreeter {
 
 	public static void main(String[] args) {
-		Greeter test= new Greeter();
+		Greeter test = new Greeter();
 		System.out.println(test.toString());
+		Greeter[] learn = new Greeter[5];
+		for (Greeter fun : learn) {
+			fun.toString();
+		}
 	}
 
 }
